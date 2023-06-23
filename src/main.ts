a9743be70b5f0b43e259e062743dc5a027e02cb0
Yurii Cherniak <juracherniak@gmail.com>
2023-06-23 22:04:38 +0200

    add basic layout

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,27 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+const servers = {
+    iceServers: [
+        {
+            urls: [
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+            ],
+        },
+    ],
+    iceCandidatePoolSize: 10,
+};
+
+let pc = new RTCPeerConnection(servers);
+
+let localStream = null;
+let remoteStream = null;
+
+const webcamButton = document.getElementById('webcamButton');
+const webcamVideo = document.getElementById('webcamVideo');
+const callButton = document.getElementById('callButton');
+const callInput = document.getElementById('callInput');
+const answerButton = document.getElementById('answerButton');
+const remoteVideo = document.getElementById('remoteVideo');
+const hangupButton = document.getElementById('hangupButton');
